e01f47250a1f3984299313465a3976309153c70d
lirui <821342264@qq.com>
2024-05-07 11:33:02 +0800

    trie

--- web/router.go
+++ web/router.go
@@ -1,20 +1,43 @@
 package web
 
 import (
-	"log"
+	"strings"
 )
 
 type router struct {
 	handlers map[string]HandlerFunc
+	roots    map[string]*node // GET POST 等method分开存放在不同的树
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{handlers: make(map[string]HandlerFunc), roots: make(map[string]*node)}
 }
 
 func (r *router) addRouter(method string, url string, handler HandlerFunc) {
-	log.Printf("Route %4s - %s", method, url)
+	parts := parsePattern(url)
+
 	key := method + "-" + url
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+
+	r.roots[method].insert(url, parts, 0)
+
 	r.handlers[key] = handler
 }
 
@@ -25,4 +48,32 @@ func (r *router) handle(c *Context) {
 	} else {
 		c.String(404, "404 NOT FOUND: %s\n", c.Path)
 	}
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
 }
